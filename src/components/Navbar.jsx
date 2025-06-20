a54ad78683105645e18c77a64b4dea04f880bd06
jindal07 <harshjindal458@gmail.com>
2025-06-20 15:03:19 +0530

    fix: Update font family in index.html and adjust color in App.css for improved styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { Brain,Sun } from 'lucide-react';
 
 function Navbar() {
   return (
-    <div className='nav flex items-center rounded-2xl shadow-2xl px-5 py-5 mx-3  h-17 bg-[radial-gradient(circle_at_30%_30%,_#702963,_#1e3a8a)] ' >
+    <div className=' nav flex items-center rounded-2xl shadow-2xl px-5 py-5 mx-3  h-17 bg-[radial-gradient(circle_at_30%_30%,_#702963,_#1e3a8a)] ' >
      <div className='logo flex items-center  gap-1'>
         <Brain className=''/>
-        <span className='text-2xl font-bold text-white ml-1'>SyntaxSniffer</span>
+        <span className=' text-2xl font-bold text-white ml-1'>SyntaxSniffer</span>
      </div>
 
     </div>
